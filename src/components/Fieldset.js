fe38a327d391b38dc8eee4832d291baa8894c046
Edin <edin.vatrich@gmail.com>
2025-05-23 20:03:56 +0200

    Added posts.js and context

--- src/components/Fieldset.js
+++ src/components/Fieldset.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useCallback, useContext } from 'react';
+import { CountContext } from '../App';
 
 const Fieldset = (props) => {
   const { post, username } = props;
+  const countContext = useContext(CountContext);
   return (
     <div className="text">
       <fieldset>
         <p>{post}</p>
-        <button>Šamar</button>
-        <button>Aplauz</button>
+        <button onClick={() => countContext.countDispatch('increment')}>
+          Šamar
+        </button>
+        <button onClick={() => countContext.countDispatch('decrement')}>
+          Aplauz
+        </button>
         <p id="ispis3"></p>
         <br></br>
         <span class="korisnik">Napisao {username} | 25/12/2020</span>

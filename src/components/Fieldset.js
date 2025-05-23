35d5d36de255098d2fe634f03f6e6de4ebd3ec74
Edin <edin.vatrich@gmail.com>
2025-05-23 20:15:42 +0200

    Added option disable/enable for buttons

--- src/components/Fieldset.js
+++ src/components/Fieldset.js
@@ -1,20 +1,38 @@
-import React, { useCallback, useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { CountContext } from '../App';
 
 const Fieldset = (props) => {
   const { post, username } = props;
-  const countContext = useContext(CountContext);
+  //const countContext = useContext(CountContext);
+
+  const { countDispatch } = useContext(CountContext);
+
+  const [isShamarDisabled, setShamarDisabled] = useState(false);
+  const [isAplauzDisabled, setAplauzDisabled] = useState(false);
+
+  const handleShamar = () => {
+    countDispatch('increment');
+    setShamarDisabled(true); // disable after click
+    setAplauzDisabled(false);
+  };
+
+  const handleAplauz = () => {
+    countDispatch('decrement');
+    setAplauzDisabled(true); // disable after click
+    setShamarDisabled(false);
+  };
+
   return (
     <div className="text">
       <fieldset>
         <p>{post}</p>
-        <button onClick={() => countContext.countDispatch('increment')}>
+        <button onClick={handleShamar} disabled={isShamarDisabled}>
           Šamar
         </button>
-        <button onClick={() => countContext.countDispatch('decrement')}>
+        <button onClick={handleAplauz} disabled={isAplauzDisabled}>
           Aplauz
         </button>
-        <p id="ispis3"></p>
+        <p id="ispis"></p>
         <br></br>
         <span class="korisnik">Napisao {username} | 25/12/2020</span>
       </fieldset>
